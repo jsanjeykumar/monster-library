29778e869c9be21c741478fb461d912b55d30647
san_j <jsanjeykumr@gmail.com>
2020-04-12 17:12:33 +0530

    comments added

--- src/App.js
+++ src/App.js
@@ -4,13 +4,29 @@ import { CardList } from "./components/card-list/card-list.component";
 import { SearchBox } from "./components/search-box/search-box.component";
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       monsters: [],
       searchField: "",
     };
+    //when using props pass the props in constructor
+    this.props = props 
   }
+  //this also works -> state as class variable
+  /* state = {
+    monsters:[]
+  } */
+
+  handleClick = () => {
+    //setstate()=>asynchronus
+    this.setState(
+      //prevState= state before this, props before this
+      (prevState, prevProps) => {},
+      () => console.log("this.state")
+    );
+    
+  };
   //when this component mount(puts it on the page)
   componentDidMount() {
     fetch(`https://jsonplaceholder.typicode.com/users`)
@@ -19,12 +35,12 @@ class App extends Component {
         this.setState({ monsters: users });
       });
   }
-  onChangeSearch = e => {
+  onChangeSearch = (e) => {
     console.log(e.target.value);
     this.setState({ searchField: e.target.value });
   };
   render() {
-    //de-Structuring => object & it properties     
+    //de-Structuring => object & it properties
     const { monsters, searchField } = this.state;
 
     let filteredMonster = monsters.filter((monster) =>
@@ -32,7 +48,7 @@ class App extends Component {
     );
 
     return (
-      //sender - state 
+      //sender - state
       //receiver - props
       <div className="App">
         <h1>Monster Library</h1>
